57f054ede2d62717d38484b55bacfff4702c904f
ufo-alien <imwe5907@gmail.com>
2025-03-12 20:44:34 +0500

    jjjkkk

---
+++ notifier.py
@@ -0,0 +1,11 @@
+def notify_admin(anomalies):
+    """
+    Уведомляет администратора об аномалиях.
+    :param anomalies: словарь с аномальными IP и количеством запросов
+    """
+    if anomalies:
+        print("Обнаружены аномалии:")
+        for ip, count in anomalies.items():
+            print(f"IP: {ip} - Запросов: {count}")
+    else:
+        print("Аномалий не обнаружено.")
